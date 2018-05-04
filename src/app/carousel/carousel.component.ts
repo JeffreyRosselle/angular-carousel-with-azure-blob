f45b7f030109f14ffb16e157c552b20c82561b11
Jeffrey Rosselle <me@jeffreyrosselle.me>
2018-05-04 22:40:33 +0200

    clean up code

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
-import * as $ from 'jquery';
 
 import { CarouselService } from './carousel.service';
 
@@ -10,25 +9,15 @@ import { CarouselService } from './carousel.service';
 })
 export class CarouselComponent implements OnInit {
     @Input() container: string;
-
-    private start = false;
+    
     urls: string[] = [];
     constructor(private carouselService: CarouselService) { }
 
     ngOnInit() {
         if (this.container !== undefined) {
             this.carouselService.get(this.container).subscribe(
-                (urls: string[]) => this.startCarousel(urls)
+                (urls: string[]) => this.urls = urls
             );
         }
-    }
-
-    private startCarousel(urls: string[]) {
-        this.urls = urls;
-        //$('.carousel').carousel('cycle');
-    }
-
-    isActive(url: string) {
-        return url === this.urls[0];
     }
 }
